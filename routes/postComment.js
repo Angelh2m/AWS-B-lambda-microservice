19c9adcaec2ef04b2ce827cb84a6501129ee1ba2
Angel Hernandez <ahm@Angels-MBP.lan1>
2019-01-12 12:03:59 -0600

    Comments working

--- routes/postComment.js
+++ routes/postComment.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const PostComment = require('../models/PostComment.model');
-const BlogUsers = require('../models/blogUsers');
+const { BlogUser } = require('../models/blogUsers');
 const moment = require('moment');
 
 // ENDPOINT: http://localhost:4000/comments/all-comments
@@ -31,126 +31,70 @@ router.post('/', async (req, res) => {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         avatar: req.body.avatar,
-        socialID: req.body.id,
+        socialID: req.body.socialID,
         exp: req.body.exp,
         location: req.body.location,
         comment: req.body.comment
     }
 
-    let userID = "";
-
     // *** Find by user by its social media ID
-
-
-    await BlogUsers
+    return BlogUser
         .find({ socialID: user.socialID })
-        .then(user => { userID = user })
-        .catch(() => { console.log('Not found') });
-
-    // *** If user does not exist register as new user
+        .then(resp => {
 
 
-    if (!userID) {
-        console.log("CREATE USER");
-        await BlogUsers
-            .create({ ...user })
-            .then(freshUser => userID = freshUser ? freshUser[0].id : null)
-            .catch(() => { console.log('ERROR WHEN CREATING') });
-    }
+            if (!resp[0].socialID) {
+                res.status(400).json({ message: 'User not registered' })
+            }
 
-    // *** Stop continious messages by users
-    let shouldIStopTheAttack = false;
+            // *** Post comment and user ID
+            PostComment
+                .find({ post: user.location })
+                .then(post => {
 
-    const value = await PostComment
-        .find({ post: user.location })
-        .then(post => {
-            if (post) {
-
-
-                post[0].comments.filter(comment => {
-
-
-                    if (user.socialID == comment.socialID) {
-
-                        let currentDate = moment().format('LLLL');
-                        // TODO SET 0 - 5 minutes
-                        let TwentyminutesLates = moment(comment.date).add(0, 'minutes').format('LLLL');
-
-                        console.log("•••••••••••••••••••••••••••••••••••", TwentyminutesLates < currentDate);
-
-                        if (TwentyminutesLates < currentDate) { return shouldIStopTheAttack = true }
+                    if (!post[0]) {
+                        res.status(400).json({ message: 'No post based on location' })
                     }
-                })
-            }
-        }).catch(() => { console.log('Error when trying to filter dates') });
 
-    console.log(value, shouldIStopTheAttack);
+                    let stopAttack;
+                    // Find post comment from the user if already commented
+                    post[0].comments.filter(comment => {
 
-    // *** Stop attack if any
-    if (shouldIStopTheAttack) {
-        return res.status(500).json({ ok: false, message: `For security reasons you canonly post every 5 minutes you can still try later` })
-    }
+                        if (user.socialID == comment.socialID) {
 
-    let postComment = {
-        socialID: user.socialID,
-        name: user.name,
-        comment: user.comment,
-        avatar: user.avatar,
-    }
-    // console.log("postComment ************************ ", postComment);
+                            let currentDate = moment().format('LLLL');
+                            // TODO SET 0 - 5 minutes
+                            let TwentyminutesLates = moment(comment.date).add(5, 'minutes').format('LLLL');
+                            // console.log("•••••••••••••••••••••••••••••••••••", TwentyminutesLates, currentDate);
+                            if (TwentyminutesLates > currentDate) { return stopAttack = true }
+                        }
+                    })
 
+                    if (stopAttack) {
+                        return res.status(403).json({ ok: false, message: "Attack stopped!" })
+                    }
 
-    let createNewPost = false;
+                    // return
+                    let postComment = {
+                        socialID: user.socialID,
+                        name: user.name,
+                        comment: user.comment,
+                        avatar: user.avatar,
+                    }
 
-    // *** Succesfully create the comment
-    await PostComment
-        .findOneAndUpdate({ post: user.location }, { "$push": { "comments": postComment } })
-        .then(updatedPost => {
-            // console.log("DONEE POSTING ", updatedPost);
+                    // return
+                    PostComment
+                        .findOneAndUpdate({ post: user.location }, { "$push": { "comments": postComment } })
+                        .then(updatedPost => {
+                            return res.status(200).json({ ok: true, updatedPost });
+                        })
 
-            if (!updatedPost) {
-                createNewPost = true
-            }
+                        .catch(err => { res.status(500).json({ message: 'There is an error with updating your post.' }) });
 
-            if (updatedPost) {
-                return res.status(200).json({ ok: true, updatedPost });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'There is an error with updating your post.'
-            });
-        });
+                }).catch(() => { console.log('Error when trying to filter dates') });
 
-    if (createNewPost !== true) {
-        return
-    }
-
-    let payload = {
-        post: user.location,
-        comments: [{ ...postComment }]
-    }
-
-    PostComment
-        .create(payload)
-        .then(newPost => res.status(201).json(newPost))
-        .catch(() => { res.status(500).json({ error: "Duplicate entry error" }); });
+        }).catch(() => { console.log('Not found') });
 })
-
-
-// router.get('/all', async (req, res) => {
-//     return BlogPost
-//         .find({}, 'comment slug')
-//         .then(posts => {
-
-//             res.status(200).json({ ok: true, posts });
-//         })
-//         .catch(err => {
-//             res.status(500).json({
-//                 message: 'There is an error with updating your post.'
-//             });
-//         });
-// })
 
 
 
